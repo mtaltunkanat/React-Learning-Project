3cfe2bb1a378ea9cfabac6653d7407a745d2fab2
mtaltunkanat <muhammedaltunkanat.kariyer@gmail.com>
2024-10-28 16:59:21 +0300

    day end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from "./logo.svg"
+//importla useeffecti
 function App() {
+  useEffect(() => {
+    if(process.env.NODE_ENV === 'production'){
+      //analytici bilgilendir
+    }
+  }, [])
   return (
     <div className="App">
-      <img src="/logo192.png" alt="" />
-      <img src={logo} alt="" />
+      <h3>{process.env.NODE_ENV}</h3>
+      {process.env.NODE_ENV === 'proudction' &&(
+        <>
+        <img src="/logo192.png" alt="" />
+        <img src={logo} alt="" />
+        </>
+      )}
     </div>
   );
 }
